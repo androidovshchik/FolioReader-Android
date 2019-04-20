59b288596b92aba30fe7ae50b683d72fea719fd1
isavnin <vladkalyuzhnyu@gmail.com>
2019-04-20 07:21:25 +0300

    changes

--- app/src/main/java/com/folioreader/android/sample/MainApplication.kt
+++ app/src/main/java/com/folioreader/android/sample/MainApplication.kt
@@ -8,23 +8,26 @@ import com.folioreader.model.locators.ReadLocator
 import com.folioreader.util.AppUtil
 import com.folioreader.util.OnHighlightListener
 import com.folioreader.util.ReadLocatorListener
-import com.yandex.metrica.YandexMetrica
-import com.yandex.metrica.YandexMetricaConfig
+import timber.log.Timber
 
 class MainApplication : Application(), OnHighlightListener, ReadLocatorListener, FolioReader.OnClosedListener {
 
     override fun onCreate() {
         super.onCreate()
+        Timber.plant(Timber.DebugTree())
         // Создание расширенной конфигурации библиотеки.
-        val metricaConfig = YandexMetricaConfig.newConfigBuilder(getString(R.string.yandex_key)).build()
+        /*val metricaConfig = YandexMetricaConfig.newConfigBuilder(getString(R.string.yandex_key)).build()
         // Инициализация AppMetrica SDK.
         YandexMetrica.activate(applicationContext, metricaConfig)
         // Отслеживание активности пользователей.
         YandexMetrica.enableActivityAutoTracking(this)
         if (!BuildConfig.DEBUG) {
-            //MobileAds.initialize(applicationContext, getString(R.string.ads_app))
-        }
+           //MobileAds.initialize(applicationContext, getString(R.string.admob_app))
+        }*/
         val folioReader = FolioReader.get()
+            .setOnHighlightListener(this)
+            .setReadLocatorListener(this)
+            .setOnClosedListener(this)
         val readLocator = getLastReadLocator()
         val config = AppUtil.getSavedConfig(applicationContext) ?: Config()
         config.allowedDirection = Config.AllowedDirection.ONLY_HORIZONTAL
@@ -35,21 +38,27 @@ class MainApplication : Application(), OnHighlightListener, ReadLocatorListener,
     }
 
     private fun getLastReadLocator(): ReadLocator? {
-        val jsonString = assets.open("Locators/LastReadLocators/last_read_locator_1.json")
-            .bufferedReader()
-            .use { it.readText() }
-        return ReadLocator.fromJson(jsonString)
+        return ReadLocator.fromJson(
+            "{\n" +
+                    "  \"bookId\": \"_simple_book\",\n" +
+                    "  \"href\": \"/OEBPS/ch03.xhtml\",\n" +
+                    "  \"created\": 1539934158390,\n" +
+                    "  \"locations\": {\n" +
+                    "    \"cfi\": \"epubcfi(/0!/4/4)\"\n" +
+                    "  }\n" +
+                    "}"
+        )
     }
 
     override fun onHighlight(highlight: HighLight?, type: HighLight.HighLightAction?) {
-
+        Timber.d("onHighlight")
     }
 
     override fun saveReadLocator(readLocator: ReadLocator?) {
-
+        Timber.d("saveReadLocator")
     }
 
     override fun onFolioReaderClosed() {
-
+        Timber.d("onFolioReaderClosed")
     }
 }
